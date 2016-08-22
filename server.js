af2b44a8f255332eb575f684e41720f4ec880d03
Frank Lin <fylin134@gmail.com>
2016-08-21 22:35:00 -0500

    full solution complete

--- server.js
+++ server.js
@@ -3,12 +3,71 @@ var app = express();
 
 var port = process.env.PORT || 8080;
 
-app.get('/:datestring', function(req, res)
+app.get('/:in_date', function(req, res)
 {	
-	var datestring = "";
-	datestring += req.params.datestring;
-	var unixStamp = Date.parse(datestring);
-	res.end(unixStamp);
+	var str_inDate = req.params.in_date;
+	var unixStamp = Date.parse(str_inDate)/1000;
+	if(/^\d+$/.test(str_inDate))
+	{
+		var date = new Date(parseInt(str_inDate, 10)*1000);
+		var int_year = date.getFullYear();
+		var str_month = getStringMonth(date.getMonth());
+		var int_day = date.getDate();
+		var str_date = "" + str_month + " " + int_day + ", " + int_year;
+
+		res.json(
+		{
+			'unix': unixStamp,
+			'natural': str_date
+		});
+	}
+	else if(Number.isNaN(unixStamp))
+	{
+		res.json(
+		{
+			'unix': null,
+			'natural': null
+		});
+	}	
+	else
+	{
+		res.json(
+		{
+			'unix': unixStamp.toString(),
+			'natural': str_inDate
+		});
+	}	
 });
 
 app.listen(port);
+
+function getStringMonth(intMonth)
+{
+	switch(intMonth)
+	{
+		case 0:
+			return "January";
+		case 1:
+			return "February";
+		case 2:
+			return "March";
+		case 3:
+			return "April";
+		case 4:
+			return "May";
+		case 5:
+			return "June";
+		case 6:
+			return "July";
+		case 7:
+			return "August";
+		case 8:
+			return "September";
+		case 9:
+			return "October";
+		case 10:
+			return "November";
+		case 11:
+			return "December";
+	}
+}
